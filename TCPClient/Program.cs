a4c0461b208eb7484c994ed3a71b0b8a06e89526
Marina120803 <petrenkomarina567@gmail.com>
2023-04-24 10:30:22 +0300

    Добавьте файлы проекта.

---
+++ TCPClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using TCPClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        client.handlerAsync();
+
+        Console.WriteLine("Hello, World!");
+    }
+}
